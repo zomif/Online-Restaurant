088fd559c6e4965c31bf6c7f29f145b84a06bedb
qu1zz-limon <e3365724@gmail.com>
2026-08-23 09:59:10 +0200

    Update app.py

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ main = Blueprint("main", __name__)
 
 @main.route("/")
 def index():
-    return render_template("main/index.html")
+    return render_template("base.html")
 
 
 auth = Blueprint("auth", __name__, url_prefix="/auth")
