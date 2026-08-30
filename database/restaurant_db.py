ba7a12868b02cd25bbc2a95d10ea2c4cce62e983
qu1zz-limon <e3365724@gmail.com>
2026-08-30 09:48:27 +0200

    Update restaurant_db.py

--- database/restaurant_db.py
+++ database/restaurant_db.py
@@ -13,12 +13,15 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password_hash = db.Column(db.String(256), nullable=False)
     role = db.Column(db.String(20), default='customer')
+    profile_picture = db.Column(db.String(255), nullable=True)
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+
 
 class FoodItem(db.Model):
     __tablename__ = 'food_items'
